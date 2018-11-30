1b4cbfe0894b3e529c83499aeac5cace28550ba5
Nwy <nwy140@gmail.com>
2018-11-30 18:56:42 +0800

    V0.0.1 Initialize Mario Prototype, implemented Movement and Jump animations and functionality

---
+++ Awesome Bird/Assets/Scripts/Helper Scripts/TagManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagManager : MonoBehaviour {
+
+	public static string PLAYER_TAG = "Player";
+	public static string BORDER_TAG = "Border";
+	public static string GROUND_TAG = "Ground";
+	public static string WARNING_TAG = "Warning";
+	public static string DIAMOND_TAG = "Diamond";
+	public static string SCORE_TAG = "Score";
+
+
+	public static string FADE_IN_ANIMATION = "FadeIn";
+	public static string JUMP_BOOL_ANIMPARAM= "Jumping";
+	public static string WALK_BOOL_ANIMPARAM= "Walking";
+
+	public static string GAME_DATA = "/GameData.dat";
+
+
+
+
+
+
+
+
+
+
+
+
+/*Awesome Bird Archive*/
+
+
+	public static string DOG_TAG = "Dog";
+	public static string FLY_TRIGGER = "Fly";
+	public static string DEAD_ANIMATION = "Dead";
+
+
+
+
+
+
+
+
+
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
