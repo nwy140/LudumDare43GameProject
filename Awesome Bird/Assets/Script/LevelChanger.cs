7ac7f98ede24d9ae2cdc7ce4a01fd3e3615fde41
Nwy <nwy140@gmail.com>
2018-12-01 21:43:31 +0800

    V0.0.2e Added Gdrive Assets

---
+++ Awesome Bird/Assets/Script/LevelChanger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+    public Animator animator;
+    private int LevelToLoad;
+    
+    
+    // Use this for initialization
+	void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(1);
+        }
+    }
+
+    
+    public void FadeToLevel(int levelIndex)
+    {
+        LevelToLoad = levelIndex;
+        animator.SetBool("TimerRight", true);
+    }
+		
+	    public void FadeLevelComplete()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}
