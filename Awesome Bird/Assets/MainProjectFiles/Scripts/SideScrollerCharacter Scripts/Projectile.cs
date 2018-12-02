bb82c547112078e8eb60d5111c6a21f8eca6a775
Nwy <nwy140@gmail.com>
2018-12-02 21:01:24 +0800

    V0.0.3h Added Rolling Ball Prefab Implemented using Projectile Script

--- Awesome Bird/Assets/MainProjectFiles/Scripts/SideScrollerCharacter Scripts/Projectile.cs
+++ Awesome Bird/Assets/MainProjectFiles/Scripts/SideScrollerCharacter Scripts/Projectile.cs
@@ -8,15 +8,16 @@ public class Projectile : MonoBehaviour {
         [HideInInspector]
         public GameObject instigator  ;
         Rigidbody2D myBody;
-        [HideInInspector]
         public bool goLeft;
         public float move_Speed = 30f;
         public float damage = 20f;
+        public float lifeSpan = 3f;
+
         // Use this for initialization
         void Awake ()
         {
             myBody = GetComponent<Rigidbody2D>();
-            Destroy(gameObject,3f); // lifespan
+            Destroy(gameObject,lifeSpan); // lifespan
         }
        
         // Update is called once per frame
@@ -32,20 +33,51 @@ public class Projectile : MonoBehaviour {
         }
 
         void OnTriggerEnter2D(Collider2D other) {
-            if(other.gameObject.tag == TagManager.ENEMY_TAG && instigator.tag == TagManager.PLAYER_TAG ){
+            if(instigator){
+
+                if(other.gameObject.tag == TagManager.ENEMY_TAG && instigator.tag == TagManager.PLAYER_TAG ){
                 SoundManager.instance.hitSoundManager.Play();
                 other.gameObject.GetComponent<EnemyHealth>().TakeDamage(damage);
                 Destroy(gameObject);
-            }
+                }
         
-            // protect null ptr // attack player as enemy
-            if(other.gameObject.tag == TagManager.PLAYER_TAG && instigator.tag == TagManager.ENEMY_TAG ){
-                SoundManager.instance.hitSoundManager.Play();
-                other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
-                Destroy(gameObject);
+                // protect null ptr // attack player as enemy
+                if(other.gameObject.tag == TagManager.PLAYER_TAG && instigator.tag == TagManager.ENEMY_TAG ){
+                    SoundManager.instance.hitSoundManager.Play();
+                    other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
+                    Destroy(gameObject);
+                }
+
+            } else if(other.gameObject.tag == TagManager.PLAYER_TAG){ // just call again
+                    SoundManager.instance.hitSoundManager.Play();
+                    other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
+                    Destroy(gameObject);                
             }
 
         }
+         void OnCollisionEnter2D(Collision2D other) {
+            if(instigator){
 
+                if(other.gameObject.tag == TagManager.ENEMY_TAG && instigator.tag == TagManager.PLAYER_TAG ){
+                SoundManager.instance.hitSoundManager.Play();
+                other.gameObject.GetComponent<EnemyHealth>().TakeDamage(damage);
+                Destroy(gameObject);
+                }
+        
+                // protect null ptr // attack player as enemy
+                if(other.gameObject.tag == TagManager.PLAYER_TAG && instigator.tag == TagManager.ENEMY_TAG ){
+                    SoundManager.instance.hitSoundManager.Play();
+                    other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
+                    Destroy(gameObject);
+                }
+
+            } else if(other.gameObject.tag == TagManager.PLAYER_TAG){ // just call again
+                    SoundManager.instance.hitSoundManager.Play();
+                    other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
+                    Destroy(gameObject);                
+            }
+            
+
+        }
         
 }
